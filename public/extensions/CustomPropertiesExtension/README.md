69d0d91f57736c98a111d79f9afd9f2e5309863e
Adam Nagy <adam_e_nagy@hotmail.com>
2020-11-30 09:40:02 +0000

    Adding CustomPropertiesExtension

---
+++ public/extensions/CustomPropertiesExtension/README.md
@@ -0,0 +1,46 @@
+# Custom Properties Extension
+
+[Demo](https://forge-extensions.autodesk.io/?extension=CustomPropertiesExtension)
+
+Extension showing custom properties in the Properties palette, based on their `dbId`.
+
+![thumbnail](extension.gif)
+
+## Setup
+
+Include the JS file on your page. This CDN is compatible with the lastest Viewer version (v7).
+
+```xml
+<script src="http://cdn.jsdelivr.net/gh/autodesk-forge/forge-extensions/public/extensions/CustomPropertiesExtension/contents/main.js"></script>
+```
+
+Load the extension passing the properties you want to add to various objects based on their `dbId`. 
+
+```javascript
+viewer.loadExtension('CustomPropertiesExtension', {
+    "properties": {
+        "1": {
+            "My Group": {
+                "My Property": "My Property Value"
+            }
+        },
+        "3": {
+            "My Other Group": {
+                "My Other Property": "My Other Property Value"
+            }
+        }
+    }
+})
+```
+
+## Configuration
+
+The `properties` contains the dbId's you assign custom properties to. 
+
+## How it works
+
+The custom propertioes will appear in the Properties palette.
+
+## Futher reading
+
+The sample is based on [this blog post](https://forge.autodesk.com/blog/adding-custom-properties-property-panel).
---
+++ public/extensions/CustomPropertiesExtension/contents/main.js
@@ -0,0 +1,70 @@
+/////////////////////////////////////////////////////////////////////
+// Copyright (c) Autodesk, Inc. All rights reserved
+// Written by Forge Partner Development
+//
+// Permission to use, copy, modify, and distribute this software in
+// object code form for any purpose and without fee is hereby granted,
+// provided that the above copyright notice appears in all copies and
+// that both that copyright notice and the limited warranty and
+// restricted rights notice below appear in all supporting
+// documentation.
+//
+// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS.
+// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC.
+// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
+// UNINTERRUPTED OR ERROR FREE.
+/////////////////////////////////////////////////////////////////////
+
+// *******************************************
+// Custom Property Panel
+// *******************************************
+class CustomPropertyPanel extends Autodesk.Viewing.Extensions.ViewerPropertyPanel {
+    constructor (viewer, options) {
+        super(viewer, options);
+        this.properties = options.properties || {};
+    }
+
+    setProperties(properties, options) {
+        Autodesk.Viewing.Extensions.ViewerPropertyPanel.prototype.setProperties.call(this, properties, options);
+
+        // add your custom properties here
+        var propsForObject = this.properties[this.propertyNodeId.toString()];
+        if (propsForObject) {
+            for (const groupName in propsForObject) {
+                const group = propsForObject[groupName];
+                for (const propName in group) {
+                    const prop = group[propName];
+                    this.addProperty(propName, prop, groupName);
+                }
+            }
+        }
+    }
+};
+
+// *******************************************
+// Custom Properties Extension
+// *******************************************
+class CustomPropertiesExtension extends Autodesk.Viewing.Extension {
+    constructor(viewer, options) {
+        super(viewer, options);
+       
+        this.panel = new CustomPropertyPanel(viewer, options);
+    }
+
+    async load() {
+        var ext = await this.viewer.getExtension('Autodesk.PropertiesManager');
+        ext.setPanel(this.panel);
+
+        return true;
+    }
+
+    async unload() {
+        var ext = await this.viewer.getExtension('Autodesk.PropertiesManager');
+        ext.setDefaultPanel();
+
+        return true;
+    }
+}
+
+Autodesk.Viewing.theExtensionManager.registerExtension('CustomPropertiesExtension', CustomPropertiesExtension);
