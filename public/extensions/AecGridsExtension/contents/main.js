c1178d297e200e5af52d3337d1b1d9c8804ef7de
Eason Kang <eason.kang@autodesk.com>
2021-06-30 13:24:08 +0800

    Improving AecGridsExtension usability

--- public/extensions/AecGridsExtension/contents/main.js
+++ public/extensions/AecGridsExtension/contents/main.js
@@ -16,15 +16,16 @@
 // UNINTERRUPTED OR ERROR FREE.
 /////////////////////////////////////////////////////////////////////
 
-class AecGridsExtension extends Autodesk.Viewing.Extension {
+class AecGrid {
     constructor(viewer, options) {
-        super(viewer, options);
-
+        this.viewer = viewer;
         this.modelBuilder = null;
         this.idPrefix = 100;
+
+        this.options = options || {};
     }
 
-    async load() {
+    async init() {
         const modelBuilderExt = await this.viewer.loadExtension('Autodesk.Viewing.SceneBuilder');
         const modelBuilder = await modelBuilderExt.addNewModel({
             conserveMemory: false,
@@ -33,28 +34,6 @@ class AecGridsExtension extends Autodesk.Viewing.Extension {
 
         this.modelBuilder = modelBuilder;
 
-        if (!this.viewer.isLoadDone()) {
-            this.viewer.addEventListener(
-                Autodesk.Viewing.GEOMETRY_LOADED_EVENT,
-                () => this.createGrids(),
-                { once: true }
-            );
-        } else {
-            this.createGrids();
-        }
-
-        return true;
-    }
-
-    unload() {
-        this.viewer.impl.unloadModel(this.modelBuilder.model);
-
-        delete this.linesMaterial;
-        this.linesMaterial = null;
-        return true;
-    }
-
-    createMaterials() {
         const matName = 'grid-line-mat';
         const linesMaterial = new THREE.LineBasicMaterial({
             color: 0xff0000,
@@ -96,7 +75,7 @@ class AecGridsExtension extends Autodesk.Viewing.Extension {
 
         const labelDbId = this.idPrefix++;
         const matName = `label-mat-${labelDbId}`;
-        const material = new THREE.MeshPhongMaterial({ map: texture });
+        const material = new THREE.MeshPhongMaterial({ map: texture, side: THREE.DoubleSide, opacity: 0.8, transparent: true });
         this.modelBuilder.addMaterial(matName, material);
         const labelMat = this.modelBuilder.findMaterial(matName);
 
@@ -123,6 +102,11 @@ class AecGridsExtension extends Autodesk.Viewing.Extension {
             const { start, end } = seg.points;
             const startPoint = new THREE.Vector3(start[0], start[1], start[2]).applyMatrix4(offsetMatrix);
             const endPoint = new THREE.Vector3(end[0], end[1], end[2]).applyMatrix4(offsetMatrix);
+
+            if (this.options.hasOwnProperty('heightOverride') && !isNaN(this.options.heightOverride)) {
+                startPoint.setZ(this.options.heightOverride);
+                endPoint.setZ(this.options.heightOverride);
+            }
 
             //grid line
             const lineGeo = new THREE.BufferGeometry();
@@ -151,22 +135,343 @@ class AecGridsExtension extends Autodesk.Viewing.Extension {
         }
     }
 
-    async createGrids() {
-        const aecdata = await this.viewer.model.getDocumentNode().getDocument().downloadAecModelData();
-        const grids = aecdata.grids;
+    async build() {
+        await this.init();
 
-        this.createMaterials();
+        let aecdata = this.viewer.model.getDocumentNode().getAecModelData();
+        if (!aecdata)
+            aecdata = await this.viewer.model.getDocumentNode().getDocument().downloadAecModelData();
+
+        const grids = aecdata.grids;
 
         for (let i = 0; i < grids.length; i++) {
             const grid = grids[i];
             this.createGrid(grid);
         }
 
-        // uncomment to prevent selection on grids
-        // const dbIds = this.modelBuilder.model.getFragmentList().fragments.fragId2dbId;
-        // const model = this.modelBuilder.model;
-        // this.viewer.lockSelection(dbIds, true, model);
+        return true;
+    }
+
+    destroy() {
+        this.viewer.impl.unloadModel(this.modelBuilder.model);
+
+        delete this.linesMaterial;
+        this.linesMaterial = null;
+
+        return true;
+    }
+
+    lockSelection() {
+        const dbIds = this.modelBuilder.model.getFragmentList().fragments.fragId2dbId;
+        const model = this.modelBuilder.model;
+        this.viewer.lockSelection(dbIds, true, model);
+    }
+
+    unlockSelection() {
+        const dbIds = this.modelBuilder.model.getFragmentList().fragments.fragId2dbId;
+        const model = this.modelBuilder.model;
+        this.viewer.unlockSelection(dbIds, model);
     }
 }
+
+const AEC_GRIDS_CHANGED_EVENT = 'aecGridsChangedEvent';
+
+class AecGridsPanel extends Autodesk.Viewing.UI.DockingPanel {
+    constructor(viewer) {
+        const options = {};
+
+        //  Height adjustment for scroll container, offset to height of the title bar and footer by default.
+        if (!options.heightAdjustment)
+            options.heightAdjustment = 70;
+
+        if (!options.marginTop)
+            options.marginTop = 0;
+
+        //options.addFooter = false;
+
+        super(viewer.container, viewer.container.id + 'AecGridsPanel', 'Grids', options);
+
+        this.container.classList.add('fpd-docking-panel');
+        this.container.classList.add('fpd-aec-grids-panel');
+        this.createScrollContainer(options);
+
+        this.viewer = viewer;
+        this.options = options;
+        this.uiCreated = false;
+
+        this.addVisibilityListener((show) => {
+            if (!show) return;
+
+            if (!this.uiCreated)
+                this.createUI();
+        });
+    }
+
+    get levelSelector() {
+        const levelExt = this.viewer.getExtension('Autodesk.AEC.LevelsExtension');
+        return levelExt && levelExt.floorSelector;
+    }
+
+    uninitialize() {
+        super.uninitialize();
+    }
+
+    createUI() {
+        this.uiCreated = true;
+
+        const div = document.createElement('div');
+
+        const treeDiv = document.createElement('div');
+        div.appendChild(treeDiv);
+        this.treeContainer = treeDiv;
+        this.scrollContainer.appendChild(div);
+
+        this.buildTree(this.levelSelector.floorData);
+    }
+
+    findLevelByName(name) {
+        const levelData = this.levelSelector.floorData;
+        return levelData.find(level => level.name.includes(name));
+    }
+
+    hoverLevelByName(name) {
+        const level = this.findLevelByName(name);
+        let levelIdx = level ? level.index : null;
+        if (levelIdx === this.levelSelector.currentFloor) {
+            levelIdx = Autodesk.AEC.FloorSelector.AllFloors;
+        }
+
+        this.levelSelector.rollOverFloor(levelIdx);
+    }
+
+    dehoverLevel() {
+        //this.levelSelector.rollOverFloor(Autodesk.AEC.FloorSelector.NoFloor);
+        this.levelSelector.rollOverFloor();
+        this.viewer.impl.invalidate(false, true, true);
+    }
+
+    buildTree(data) {
+        const nodes = [];
+
+        for (let i = 0; i < data.length; i++) {
+            const node = {
+                id: data[i].index,
+                type: 'levels',
+                text: data[i].name
+            }
+            nodes.push(node);
+        }
+
+        console.log(nodes);
+
+        $(this.treeContainer)
+            .jstree({
+                core: {
+                    data: nodes,
+                    multiple: true,
+                    themes: {
+                        icons: false,
+                        name: 'default-dark'
+                    }
+                },
+                sort: function (a, b) {
+                    const a1 = this.get_node(a);
+                    const b1 = this.get_node(b);
+                    return (a1.text > b1.text) ? 1 : -1;
+                },
+                checkbox: {
+                    keep_selected_style: false,
+                    //three_state: false,
+                    deselect_all: true,
+                    cascade: 'none'
+                },
+                types: {
+                    levels: {},
+                    sheets: {}
+                },
+                plugins: ['types', 'checkbox', 'sort', 'wholerow'],
+            })
+            .on('open_node.jstree', (e, data) => {
+                const node = data.instance.get_node(data.node, true);
+                if (!node) {
+                    return;
+                }
+
+                node.siblings('.jstree-open').each(function () {
+                    data.instance.close_node(this, 0);
+                });
+            })
+            .on('hover_node.jstree', async (e, data) => {
+                let level = null;
+                if (data.node.type === 'levels') {
+                    level = data.node.text;
+                } else {
+                    level = data.instance.get_node(data.node.parent)?.text;
+                }
+
+                this.hoverLevelByName(level);
+            })
+            .on('dehover_node.jstree', async (e, data) => {
+                this.dehoverLevel();
+            })
+            .on('changed.jstree', async (e, data) => {
+                // console.log(e, data);
+                if (!data.node || !data.node.type) {
+                    return;
+                }
+
+                if (data.action === 'select_node') {
+                    if (data.node.type === 'levels') {
+                        const level = data.node.text;
+                        this.viewer.dispatchEvent({
+                            type: AEC_GRIDS_CHANGED_EVENT,
+                            action: 'attach',
+                            level: this.findLevelByName(level)
+                        });
+                    }
+                } else {
+                    if (data.node.type === 'levels') {
+                        const level = data.node.text;
+                        this.viewer.dispatchEvent({
+                            type: AEC_GRIDS_CHANGED_EVENT,
+                            action: 'detach',
+                            level: this.findLevelByName(level)
+                        });
+                    }
+                }
+            });
+    }
+}
+
+class AecGridsExtension extends Autodesk.Viewing.Extension {
+    constructor(viewer, options) {
+        super(viewer, options);
+
+        this.grids = [];
+        this.uiCreated = false;
+        this.panel = null;
+
+        this.createUI = this.createUI.bind(this);
+        this.onToolbarCreated = this.onToolbarCreated.bind(this);
+        this.onGridChanged = this.onGridChanged.bind(this);
+    }
+
+    createUI() {
+        this.uiCreated = true;
+
+        const viewer = this.viewer;
+
+        const aecGridsPanel = new AecGridsPanel(viewer);
+        viewer.addPanel(aecGridsPanel);
+        this.panel = aecGridsPanel;
+
+        const aecGridsToolButton = new Autodesk.Viewing.UI.Button('toolbar-aecGridsTool');
+        aecGridsToolButton.setToolTip('Grids');
+        aecGridsToolButton.setIcon('adsk-icon-documentModels');
+        aecGridsToolButton.onClick = function () {
+            aecGridsPanel.setVisible(!aecGridsPanel.isVisible());
+        };
+
+        const subToolbar = new Autodesk.Viewing.UI.ControlGroup('toolbar-fpd-tools');
+        subToolbar.addControl(aecGridsToolButton);
+        subToolbar.aecGridsToolButton = aecGridsToolButton;
+        this.subToolbar = subToolbar;
+
+        viewer.toolbar.addControl(this.subToolbar);
+
+        aecGridsPanel.addVisibilityListener(function (visible) {
+            if (visible)
+                viewer.onPanelVisible(aecGridsPanel, viewer);
+
+            aecGridsToolButton.setState(visible ? Autodesk.Viewing.UI.Button.State.ACTIVE : Autodesk.Viewing.UI.Button.State.INACTIVE);
+        });
+    }
+
+    onToolbarCreated() {
+        if (!this.uiCreated)
+            this.createUI();
+    }
+
+    async onGridChanged(event) {
+        let grid = null;
+        switch (event.action) {
+            case 'attach':
+                grid = new AecGrid(this.viewer, { level: event.level, heightOverride: event.level.zMin });
+                await grid.build();
+                this.grids.push(grid);
+                break;
+            case 'detach':
+                const idx = this.grids.findIndex(g => g.options.level.name === event.level.name);
+                if (idx <= -1)
+                    return;
+
+                const removed = this.grids.splice(idx, 1);
+                grid = removed[0];
+                grid.destroy();
+                break;
+        }
+    }
+
+    async load() {
+        // Pre-load level extension 
+        await this.viewer.model.getDocumentNode().getDocument().downloadAecModelData();
+        await this.viewer.loadExtension('Autodesk.AEC.LevelsExtension', { doNotCreateUI: true });
+
+        this.viewer.addEventListener(
+            AEC_GRIDS_CHANGED_EVENT,
+            this.onGridChanged
+        );
+
+        return true;
+    }
+
+    unload() {
+        while (this.grids.length > 0) {
+            let grid = this.grids.pop();
+            grid.destroy();
+        }
+
+        if (this.panel) {
+            this.viewer.removePanel(this.panel);
+            this.panel.uninitialize();
+            delete this.panel;
+            this.panel = null;
+        }
+
+        this.viewer.removeEventListener(
+            AEC_GRIDS_CHANGED_EVENT,
+            this.onGridChanged
+        );
+
+        if (this.subToolbar) {
+            this.viewer.toolbar.removeControl(this.subToolbar);
+            delete this.subToolbar.adnSheetsBrowserButton;
+            this.subToolbar.adnSheetsBrowserButton = null;
+            delete this.subToolbar;
+            this.subToolbar = null;
+        }
+
+        return true;
+    }
+
+    // async createGrids() {
+    //     const aecdata = await this.viewer.model.getDocumentNode().getDocument().downloadAecModelData();
+    //     const levels = aecdata.levels;
+
+    //     const offsetMatrix = this.viewer.model.getModelToViewerTransform() || new THREE.Matrix4().identity();
+
+    //     for (let i = 0; i < levels.length; i++) {
+    //         const level = levels[i];
+    //         const elevation = new THREE.Vector3(0, 0, level.elevation).applyMatrix4(offsetMatrix);
+    //         const grid = new AecGrid(this.viewer, { heightOverride: elevation.z });
+    //         await grid.build();
+
+    //         this.grids.push(grid);
+    //     }
+    // }
+}
+
+AutodeskNamespace('AecGridsExtension');
+AecGridsExtension.AEC_GRIDS_CHANGED_EVENT = AEC_GRIDS_CHANGED_EVENT;
 
 Autodesk.Viewing.theExtensionManager.registerExtension('AecGridsExtension', AecGridsExtension);
